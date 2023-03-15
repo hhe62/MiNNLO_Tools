11e6df12ba41258663cab02b9b117475fd4dac82
hehe <he.he@cern.ch>
2023-03-15 14:08:02 +0100

    Script to fix comments and fix open <event> tag and missing end line issue in LHE files

--- fixLHE.py
+++ fixLHE.py
@@ -1,6 +1,7 @@
 import sys,re,pdb
 
-# file name should contain "pre" to indicate before fixing
+# file name needs to contain "pre" to indicate "before fixing", e.g. using mv xx.LHE prexx.LHE first
+# Then the output file will become back to xx.LHE
 fn = sys.argv[1]
 fin = open(fn)
 fout = open(fn.replace("pre",""),"w")
@@ -8,9 +9,9 @@ lstore = []
 store = False
 noEndLine = True
 for line in fin:
+   
     #search for pattern #---...--
-  
-    line2 = re.sub("^#-+$","#",line) #this keeps \n in the line as it is
+    line2 = re.sub("^#-+$","#",line) #this keeps \n in the line as it is, and changes "#-..." to "#"
     
     if "<event>" in line2:
         store = True
@@ -18,7 +19,7 @@ for line in fin:
     if store:
         lstore.append(line2)
         
-        #Don't write content in between event tags until seeing end tag 
+        #Suppress writing content in between event tags until seeing end tag 
         if "</event>" in line2:
             store = False
             for sl in lstore:
@@ -32,10 +33,10 @@ for line in fin:
     if "</LesHouchesEvents>" in line:
         noEndLine = False
 
-    #skip comments after this line. If event tag not closed, then don't end yet
+    #skip comments after this line. If event tag not closed, then continue without exiting
     if not store and "</LesHouchesEvents>" in line:
         sys.exit() 
 
-# At the end of line, if event tags still not close or no endline due to other reason, write end line
+# At the end of line, if event tags still not close (suppressed writing) or no end line due to other reasons, write end line
 if store or noEndLine:
     fout.write("</LesHouchesEvents>\n")
